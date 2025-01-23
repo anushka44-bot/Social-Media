dfc912024f937345edec007d7975c6390aa79d21
Anushka Bag <bag82262@gmail.com>
2025-01-23 20:03:25 +0530

    Main

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,11 @@
 import "./App.css";
-import Create_post from "./Components/Create _post";
-import Footer from "./Components/Footer";
-import Header from "./Components/Header";
-import Sidebar from "./Components/Sidebar";
-import PostList from "./Components/PostList";
-import { useState } from "react";
-import PostListProvider from "./Store/Post_list_store";
+import Signup from "./Components/Signup";
 
 function App() {
-  const [selectedTab, setSelectedTab] = useState("Home");
-
   return (
-    <PostListProvider>
-      <div className="app-container">
-        <Sidebar selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
-        <div className="content">
-          <Header />
-          {selectedTab === "Home" ? <PostList /> : <Create_post />}
-          <Footer />
-        </div>
-      </div>
-    </PostListProvider>
+    <>
+      <Signup />
+    </>
   );
 }
 
